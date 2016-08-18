76dbd71c203bfc335e2c2d8554b59b33b9a06a92
lkk <kluneev@gmail.com>
2016-08-18 13:10:55 +0300

    Initial commit

---
+++ vacation/vacation.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+def hotel_cost(nigths):
+    return 140 * nigths
+
+def plane_ride_cost(city):
+    if city == 'Charlotte':
+        return 183
+    elif city == 'Tampa':
+        return 220
+    elif city == 'Pittsburgh':
+        return 222
+    elif city == 'Los Angeles':
+        return 475
